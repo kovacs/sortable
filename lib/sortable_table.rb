2257c73372a431ca9fef3d014c437f595c7e45e2
Michael Kovacs <kovacs@gmail.com>
2012-07-06 15:44:04 -0700

    disable test env init until it is rails 3 ocmpatible

--- lib/sortable_table.rb
+++ lib/sortable_table.rb
@@ -5,19 +5,19 @@ module SortableTable
   VIEW_PATH = File.join(File.dirname(__FILE__), 'views')
   ActionController::Base.append_view_path(VIEW_PATH) unless ActionController::Base.view_paths.include?(VIEW_PATH)
 
-  if Rails.env == 'test'
-    # only load the example files/routes in development mode
-    # TODO easy way to load all controller/model paths instead? Don't know off the top of my head but am sure it's easy
-    require File.join(File.dirname(__FILE__), 'example', 'controllers', 'cablecar', 'users_controller')
-    require File.join(File.dirname(__FILE__), 'example', 'models', 'cablecar', 'user')
-    require File.join(File.dirname(__FILE__), 'example', 'models', 'cablecar', 'contact_info')
-
-    # install the routes
-    require File.join(File.dirname(__FILE__), 'test', 'example_test_routing')
-
-    VIEW_PATH = File.join(File.dirname(__FILE__), 'example', 'views')
-    ActionController::Base.append_view_path(VIEW_PATH) unless ActionController::Base.view_paths.include?(VIEW_PATH)
-  end
+  # if Rails.env == 'test'
+  #   # only load the example files/routes in development mode
+  #   # TODO easy way to load all controller/model paths instead? Don't know off the top of my head but am sure it's easy
+  #   require File.join(File.dirname(__FILE__), 'example', 'controllers', 'cablecar', 'users_controller')
+  #   require File.join(File.dirname(__FILE__), 'example', 'models', 'cablecar', 'user')
+  #   require File.join(File.dirname(__FILE__), 'example', 'models', 'cablecar', 'contact_info')
+  # 
+  #   # install the routes
+  #   require File.join(File.dirname(__FILE__), 'test', 'example_test_routing')
+  # 
+  #   VIEW_PATH = File.join(File.dirname(__FILE__), 'example', 'views')
+  #   ActionController::Base.append_view_path(VIEW_PATH) unless ActionController::Base.view_paths.include?(VIEW_PATH)
+  # end
 
   require 'sortable_table/sortable_core'
   require 'sortable_table/sortable_helper'
